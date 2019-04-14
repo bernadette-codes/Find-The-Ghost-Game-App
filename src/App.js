443b39494819cfcbbc919bc439daf7054f98373e
Bernadette <bernadette@englemaninvest.com>
2019-04-14 02:06:10 -0400

    Move functions to own file

--- src/App.js
+++ src/App.js
@@ -1,71 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
+import Mechanics from "./components/mechanics";
 import Nav from "./components/nav";
 import MainContent from "./components/mainContent";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      heightLoc: 0,
-      widthLoc: 0,
-      startButton: true,
-      ghost: false,
-      ghostFinal: false
-    };
-  }
-
-  startGame = () => {
-    this.setState({
-      startButton: false
-    });
-    this.showGost();
-
-    // Show Final Ghost
-    setTimeout(
-      function() {
-        this.setState({
-          ghost: false,
-          ghostFinal: true
-        });
-      }.bind(this),
-      16000
-    );
-  };
-
-  clickGhost = () => {
-    this.setState({
-      ghost: false
-    });
-    this.showGost();
-  };
-
-  showGost = () => {
-    setTimeout(
-      function() {
-        this.newLoc();
-        this.setState({
-          ghost: true
-        });
-      }.bind(this),
-      800
-    );
-  };
-
-  newLoc = () => {
-    let y = Math.floor(Math.random() * this.props.height + 1);
-    let x = Math.floor(Math.random() * this.props.width + 1);
-
-    this.setState({
-      heightLoc:
-        y <= 150 ? y + 151 : y >= this.props.height - 150 ? y - 151 : y,
-      widthLoc: x <= 150 ? x + 151 : x >= this.props.width - 150 ? x - 151 : x
-    });
-  };
-
-  reset = () => {
-    window.location.reload();
-  };
-
+class App extends Mechanics {
   render() {
     const { startButton, ghost, heightLoc, widthLoc, ghostFinal } = this.state;
 
---
+++ src/components/mechanics.js
@@ -0,0 +1,68 @@
+import { Component } from "react";
+
+class Mechanics extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      heightLoc: 0,
+      widthLoc: 0,
+      startButton: true,
+      ghost: false,
+      ghostFinal: false
+    };
+  }
+
+  startGame = () => {
+    this.setState({
+      startButton: false
+    });
+    this.showGost();
+
+    // Show Final Ghost
+    setTimeout(
+      function() {
+        this.setState({
+          ghost: false,
+          ghostFinal: true
+        });
+      }.bind(this),
+      16000
+    );
+  };
+
+  clickGhost = () => {
+    this.setState({
+      ghost: false
+    });
+    this.showGost();
+  };
+
+  showGost = () => {
+    setTimeout(
+      function() {
+        this.newLoc();
+        this.setState({
+          ghost: true
+        });
+      }.bind(this),
+      800
+    );
+  };
+
+  newLoc = () => {
+    let y = Math.floor(Math.random() * this.props.height + 1);
+    let x = Math.floor(Math.random() * this.props.width + 1);
+
+    this.setState({
+      heightLoc:
+        y <= 150 ? y + 151 : y >= this.props.height - 150 ? y - 151 : y,
+      widthLoc: x <= 150 ? x + 151 : x >= this.props.width - 150 ? x - 151 : x
+    });
+  };
+
+  reset = () => {
+    window.location.reload();
+  };
+}
+
+export default Mechanics;
